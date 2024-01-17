e5d0a549f98761831fb002d8cebae1e4fa4c38d0
Alan Hayes <ahayes@dotdash.com>
2024-01-16 19:35:47 -0500

    url verification

--- packages/functions/lambda/events.go
+++ packages/functions/lambda/events.go
@@ -1,17 +1,40 @@
 package main
 
 import (
+	"encoding/json"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func Handler(request events.APIGatewayV2HTTPRequest) (events.APIGatewayProxyResponse, error) {
+type url_verification struct {
+	Token     string `json:"token"`
+	Challenge string `json:"challenge"`
+	Type      string `json:"type"`
+}
+
+func create_url_verification(request_body string) url_verification {
+	url_verification := url_verification{}
+	json.Unmarshal([]byte(request_body), &url_verification)
+	return url_verification
+}
+
+func events_handler(request events.APIGatewayV2HTTPRequest) (events.APIGatewayProxyResponse, error) {
+	url_verification := create_url_verification(request.Body)
+	switch url_verification.Challenge {
+	case "":
+		return events.APIGatewayProxyResponse{
+			Body:       request.Body,
+			StatusCode: 200,
+		}, nil
+	}
+
 	return events.APIGatewayProxyResponse{
-		Body:       "Hello, World! Your request was received at " + request.RequestContext.Time + ".",
+		Body:       request.Body,
 		StatusCode: 200,
 	}, nil
 }
 
 func main() {
-	lambda.Start(Handler)
+	lambda.Start(events_handler)
 }
